8d7aca0bd71433b50ce01f6e2f06f406362267b9
Matt Evans <mattevo10@gmail.com>
2022-06-01 16:52:01 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router"
+import NavComp from "./components/NavComp.vue"
+</script>
+
+<template>
+  <div class="container">
+    <header></header>
+    <NavComp />
+    <RouterView />
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import sourceData from "@/data.json"
+import { ref } from "vue"
+const data = ref({
+  destinations: sourceData.destinations
+})
+</script>
+
+<template>
+  <div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link v-for="destination in data.destinations" :key="destination.id" :to="destination.slug">
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" alt="destination.name" />
+      </router-link>
+    </div>
+  </div>
+</template>
